c18c11db6bf64e81ca016b507051a62247cdf8c6
Jefferson Marques <98566026+mrqssjeff@users.noreply.github.com>
2022-03-19 15:53:39 -0300

    aulas 11 e 12

---
+++ cursoemvideo/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 79
+console.log(`A velocidade do seu carrro é ${vel} km/h`)
+if (vel > 60){ // Condição simples
+    console.log('Você ultrapassou a velocidade permitida! MULTADO!')
+}
+console.log('Dirija sempre com cinto de segurança!')
