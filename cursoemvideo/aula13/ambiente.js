c6a41e1fbeb895c67991e31338b3017022d165b3
Jefferson Marques <98566026+mrqssjeff@users.noreply.github.com>
2022-03-21 21:17:19 -0300

    main

---
+++ cursoemvideo/aula13/ambiente.js
@@ -0,0 +1,11 @@
+var c = 1
+while (c < 6){
+    console.log('Hello, world!')
+    c++
+}
+
+var cont = 1
+do {
+    console.log('Olá, Mundo!')
+    cont++
+} while(cont < 6)
---
+++ cursoemvideo/aula14/ambiente.js
@@ -0,0 +1,5 @@
+console.log('Contando até 10...')
+for (var c = 1; c <= 10; c++){
+    console.log(c)
+}
+console.log('Fim!')
