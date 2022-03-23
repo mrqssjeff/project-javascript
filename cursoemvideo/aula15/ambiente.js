24de880f6cb007dca9eeb26bfac19d874eadec46
Jefferson Marques <98566026+mrqssjeff@users.noreply.github.com>
2022-03-23 12:36:20 -0300

    fim das aulas

---
+++ cursoemvideo/aula15/ambiente.js
@@ -0,0 +1,11 @@
+let num = [7, 8, 2, 3, 5]
+num.push(1)
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+let pos = num.indexOf(4)
+if (pos == -1){
+    console.log('O valor não foi encontrado')
+}else{
+    console.log(`O valor 8 está na posição ${pos}`)
+}
---
+++ cursoemvideo/aula15/vetornatela.js
@@ -0,0 +1,9 @@
+let valores = [1, 0, 4, 8, 5, 9, 6]
+console.log('Steins;Gate')
+/*
+for(let pos = 0; pos < valores.length; pos++){
+    console.log(valores[pos])
+}*/
+for(let pos in valores){
+    console.log(valores[pos])
+}
